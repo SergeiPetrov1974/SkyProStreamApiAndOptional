a9e0d5d609fc8d063aa0fb28efecd21644ae2bd0
Сергей <040620@mail.ru>
2021-12-15 18:16:28 +0300

    Initial commit

---
+++ src/test/java/pro/sky/skyprostreamapiandoptional/SkyProStreamApiAndOptionalApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.skyprostreamapiandoptional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyProStreamApiAndOptionalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
